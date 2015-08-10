fe8fb7abe290dbb809ee50a0d1d35c1dab30f11e
U-DASH2\asus computer <kuangdash@126.com>
2015-08-10 14:39:26 +0200

    localization

--- themes/default/resources/js/main.js
+++ themes/default/resources/js/main.js
@@ -16,46 +16,47 @@ $('.ds-label').click(function(){
 
 /* table-of-contents */
 window.ego_toc = $('#text-table-of-contents ul li');
-window.ego_toc_h = $('#table-of-contents h2');
-window.ego_toc_h_text = $('#table-of-contents h2').text();
-window.ego_n = 0;
-window.ego_tmp = ego_n;
-window.ego_head = $('div[id*=\'text-orgheadline\']');
-$(window).scroll(function () {
-    var startPoint=0;
-    var endPoint=ego_head.length-1;
-    var offsetValue=window.pageYOffset+60;
-    if(ego_head.eq(ego_tmp).offset().top>offsetValue || offsetValue>ego_head.eq((ego_tmp+1)>(ego_head.length-1)?(ego_head.length-1):(ego_tmp+1)).offset().top){
-        while((startPoint+1) < endPoint){
-            if(ego_head.eq(Math.floor((startPoint+endPoint)/2)).offset().top > offsetValue){
-                endPoint = Math.floor((startPoint+endPoint)/2);
+if(0 != window.ego_toc.length){
+    window.ego_toc_h = $('#table-of-contents h2');
+    window.ego_toc_h_text = $('#table-of-contents h2').text();
+    window.ego_n = 0;
+    window.ego_tmp = ego_n;
+    window.ego_head = $('div[id*=\'text-orgheadline\']');
+    $(window).scroll(function () {
+        var startPoint=0;
+        var endPoint=ego_head.length-1;
+        var offsetValue=window.pageYOffset+60;
+        if(ego_head.eq(ego_tmp).offset().top>offsetValue || offsetValue>ego_head.eq((ego_tmp+1)>(ego_head.length-1)?(ego_head.length-1):(ego_tmp+1)).offset().top){
+            while((startPoint+1) < endPoint){
+                if(ego_head.eq(Math.floor((startPoint+endPoint)/2)).offset().top > offsetValue){
+                    endPoint = Math.floor((startPoint+endPoint)/2);
+                }
+                else if(ego_head.eq(Math.floor((startPoint+endPoint)/2)).offset().top < offsetValue){
+                    startPoint = Math.floor((startPoint+endPoint)/2);
+                }
+                else{
+                    break;
+                }
             }
-            else if(ego_head.eq(Math.floor((startPoint+endPoint)/2)).offset().top < offsetValue){
-                startPoint = Math.floor((startPoint+endPoint)/2);
+            if(offsetValue>ego_head.eq(ego_head.length-1).offset().top){
+                ego_n=ego_head.length-1;
             }
             else{
-                break;
+                ego_n = startPoint;
             }
-        }
-        if(offsetValue>ego_head.eq(ego_head.length-1).offset().top){
-            ego_n=ego_head.length-1;
-        }
-        else{
-            ego_n = startPoint;
-        }
 
-        ego_toc.eq(ego_tmp).children('a').css('color', '#ffff00');
-        ego_tmp = ego_n;
-        ego_toc.eq(ego_tmp).children('a').css('color', '#22ff22');
-        if(window.pageYOffset < 10){
-            ego_toc_h[0].textContent = ego_toc_h_text;
+            ego_toc.eq(ego_tmp).children('a').css('color', '#ffff00');
+            ego_tmp = ego_n;
+            ego_toc.eq(ego_tmp).children('a').css('color', '#22ff22');
+            if(window.pageYOffset < 10){
+                ego_toc_h[0].textContent = ego_toc_h_text;
+            }
+            else{
+                ego_toc_h[0].textContent = ego_toc.eq(ego_tmp)[0].children.item(0).textContent;
+            }
+            //ego_n = parseInt(ego_str.slice(-1));
         }
-        else{
-            ego_toc_h[0].textContent = ego_toc.eq(ego_tmp)[0].children.item(0).textContent;
-        }
-        //ego_n = parseInt(ego_str.slice(-1));
-    }
-});
+    });}
 
 /* floating card */
 function popupActivate (evt) {
@@ -69,7 +70,7 @@ function popupActivate (evt) {
     if(evt.target.firstChild.parentNode.nextSibling.tagName == "SUP"){
         var footRef = evt.target.nextSibling.childNodes[0].id;
         var docNode = document.getElementById("fn."+footRef.slice(-1));
-        var nodeNew = docNode.parentNode.parentNode.lastChild.cloneNode(true);
+        var nodeNew = docNode.parentNode.parentNode.cloneNode(true);
         balloon.replaceChild(nodeNew,balloon.lastChild);
         balloon.style.visibility="visible";
     }
