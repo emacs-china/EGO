8d14dfda242a3f41727a465568a9fcd273c2bf65
U-DASH2\kuangdash <kuangdash@126.com>
2015-09-19 06:35:10 +0200

    EGO configure

--- ego.el
+++ ego.el
@@ -23,30 +23,30 @@
 
 ;;; Commentary:
 
-;; ego is a static site generator based on org mode.
+;; EGO is a static site generator based on org mode.
 
 ;; 1. Sources:   https://github.com/emacs-china/ego
 ;; 2. Documents: http://emacs-china.github.io/ego
 
-;; ego is a fork of Kelvin H's org-page (https://github.com/kelvinh/org-page),
+;; EGO is a fork of Kelvin H's org-page (https://github.com/kelvinh/org-page),
 ;; and provides similar features as org-page, the main differents is as follow:
 
 ;; 1. org-page focus on personal blog while ego is main used to
 ;;    generate small project website.
 ;; 2. org-page use many customizable variables to configure org-page
-;;    while org-website use an `org-publish-project-alist' style
-;;    alist to adjust org-website's behaver.
+;;    while EGO use an `org-publish-project-alist' style
+;;    alist to adjust EGO's behaver.
 
-;;    Managing multi-site configs in an emacs session with org-website is more
+;;    Managing multi-site configs in an emacs session with EGO is more
 ;;    simple than with org-page.
-;; 3. org-website can deal with "increment" or "inherit" themes.
+;; 3. EGO can deal with "increment" or "inherit" themes.
 ;;
 ;;    A "increment" theme is a mod theme which only include changed template,
 ;;    css and other files, the files same with base theme doesn't include.
 
-;;    ego autosearch the same files from base theme when use "increment"
+;;    EGO autosearch the same files from base theme when use "increment"
 ;;    theme.
-;; 4. org-website include a tiny emacs web server, which can be used to test publish.
+;; 4. EGO include a tiny emacs web server, which can be used to test publish.
 
 
 ;;; Code:
